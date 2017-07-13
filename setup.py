83be9d6b338edd0db4a4f9c34e06ef03892ba7f4
Ashley Sommer <ashleysommer@gmail.com>
2017-07-13 16:36:16 +1000

    Modify setup.py Change version number

--- setup.py
+++ setup.py
@@ -1,10 +1,9 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 # flake8: noqa
 from __future__ import unicode_literals
 
 import re
-import sys
 
 from setuptools import setup, find_packages
 
@@ -80,7 +79,7 @@ setup(
         'dev': dev_require,
     },
     license='MIT',
-    use_2to3=True,
+    use_2to3=False,
     zip_safe=False,
     keywords='',
     classifiers=[
@@ -91,11 +90,7 @@ setup(
         'Intended Audience :: Developers',
         'Topic :: System :: Software Distribution',
         'Programming Language :: Python',
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.3',
-        'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: Implementation :: PyPy',
